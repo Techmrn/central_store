87f43fa26080fbffca72147ef6482db79e45daee
unknown <maheshrnair007@gmail.com>
2026-07-25 23:20:17 +0530

    Added category CRUD ui Partially

--- app/routers/ui/category.py
+++ app/routers/ui/category.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, Depends, Request
-from fastapi.responses import HTMLResponse
+from fastapi import APIRouter, Depends, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from sqlalchemy.orm import Session
 
 from app.core.db import get_db
 from app.core.templates import templates
 
-from app.crud.category import get_all_categories
+from app.crud.category import get_all_categories,create_category
+from app.schemas.category import CategoryCreate
+from app.models.enums import Category_Type
 
 router = APIRouter(
     prefix="/category",
@@ -59,3 +61,38 @@ def new_category(
             "page_title": "New Category",
         },
     )
+
+@router.post("/new")
+def save_category(
+    request: Request,
+    code: str = Form(...),
+    name: str = Form(...),
+    type: Category_Type = Form(...),
+    db: Session = Depends(get_db),
+):
+    category = CategoryCreate(
+        code=code,name=name,type=type,
+    )
+
+    try:
+        create_category(db=db, category=category)
+
+        return RedirectResponse(
+            url="/category/",
+            status_code=303,
+        )
+
+    except ValueError as e:
+
+        return templates.TemplateResponse(
+            request=request,
+            name="category/form.html",
+            context={
+                "request": request,
+                "page_title": "New Category",
+                "error": str(e),
+                "category": category,
+            },
+        )
+
+
