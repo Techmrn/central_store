7e572714fa52ef80d3b29f332abc08dd7fb31488
Mahesh <maheshrnair007@gmail.com>
2026-08-19 13:26:33 +0530

    Fix items table is_temporary migration and dynamic DB URL config

--- alembic/env.py
+++ alembic/env.py
@@ -9,6 +9,12 @@ from alembic import context
 # access to the values within the .ini file in use.
 config = context.config
 
+from dotenv import load_dotenv
+import os
+load_dotenv()
+# Replace the config url with env variable
+config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
